11b9e0e0a006279f473a8ee236ed7ef681610f7e
1mmanuelKant <joselisboaa05@gmail.com>
2022-07-18 13:30:49 -0300

    cellphoto animation

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 window.addEventListener('DOMContentLoaded', () =>{
-  const cellphone = document.querySelector('.cellphone img');
-  
-  //aplicação 2: um transition no css e z-index igual a 100
-  // e ao invés de atribuir o src à cellphone atribui a classe 
   
   (function() {
     let i = 0;
-    let pics = ['./assets/mulher-e-gato.png', './assets/imginsta.png', './assets/reels.png', './assets/chat.png']
     function toggle() {
-      cellphone.src = pics[i];
-      cellphone.getAttribute('src', cellphone.src)
-      i = (i + 1) % pics.length;
+      console.log(i)
+      if(i >= 1){
+        const cellphone = document.querySelectorAll('.cellphone img')[i - 1];
+        cellphone.classList.remove('active')
+      } else if(i === 0){
+        const cellphone = document.querySelectorAll('.cellphone img')[3];
+        cellphone.classList.remove('active')
+      }
+      const cellphone = document.querySelectorAll('.cellphone img')[i];
+      cellphone.classList.add('active')
+      i = (i + 1) % 4;
     }
     toggle()
     setInterval(toggle, 3500);
